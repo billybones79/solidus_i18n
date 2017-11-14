ed6dfb01a3a24fac8316f3faf064af3036df7071
billybones79 <alexandredt79@gmail.com>
2017-11-14 16:15:18 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Spree::Core::Engine.routes.draw do
   # from routing-filter gem
-  filter :locale
-
+ 
   post '/locale/set', to: 'locale#set', defaults: { format: :json }, as: :set_locale
 
   namespace :admin do
